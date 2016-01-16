571640af441740a189d418cd041c83a9586fd057
Team2473 <frc2473@gmail.com>
2016-01-15 17:11:30 -0800

    Important comments

--- Grappler.java
+++ Grappler.java
@@ -5,39 +5,42 @@ import edu.wpi.first.wpilibj.CANTalon.ControlMode;
 import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
 
 public class Grappler {
-	CANTalon arm;
-	CANTalon elevator;
-	CANTalon reel;
 	
-	public Grappler(){
-		//change IDs
-		arm = new CANTalon(0);
-		elevator = new CANTalon(0);
-		reel = new CANTalon(0);
-	}
-	public void teleopInit(){
-		setEncoder(arm);
-		setEncoder(elevator);
-		setEncoder(reel);
-	}
-	public void teleop(){
-		//wait for an input then move motors
-		
-	}
-	public void armLaunchMotor() {
-		//1st stage
-	}
-	public void elevatorMotor() {
-		//2nd stage
-		
-	}
-	public void reelingMotor() {
-		//3rd stage: 2 feet up
-	}
-	private void setEncoder(CANTalon tal) {
-		tal.changeControlMode(ControlMode.Position);
-		tal.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-		tal.setPosition(0);
-		tal.enableControl();
-	}
+	
+	//consult with vishal over the cantalon format
+//	CANTalon arm;
+//	CANTalon elevator;
+//	CANTalon reel;
+//	
+//	public Grappler(){
+//		//change IDs
+//		arm = new CANTalon(0);
+//		elevator = new CANTalon(0);
+//		reel = new CANTalon(0);
+//	}
+//	public void teleopInit(){
+//		setEncoder(arm);
+//		setEncoder(elevator);
+//		setEncoder(reel);
+//	}
+//	public void teleop(){
+//		//wait for an input then move motors
+//		
+//	}
+//	public void armLaunchMotor() {
+//		//1st stage
+//	}
+//	public void elevatorMotor() {
+//		//2nd stage
+//		
+//	}
+//	public void reelingMotor() {
+//		//3rd stage: 2 feet up
+//	}
+//	private void setEncoder(CANTalon tal) {
+//		tal.changeControlMode(ControlMode.Position);
+//		tal.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+//		tal.setPosition(0);
+//		tal.enableControl();
+//	}
 }
