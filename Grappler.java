5cc4006c97f3b0ff49b5e5da0aad02059bf6f80a
Team2473 <frc2473@gmail.com>
2016-01-15 16:51:30 -0800

    Created method stubs and basic strategy

--- Grappler.java
+++ Grappler.java
@@ -1,5 +1,30 @@
 package org.usfirst.frc.team2473.robot;
 
+import edu.wpi.first.wpilibj.CANTalon;
+
 public class Grappler {
+	CANTalon arm;
+	CANTalon elevator;
+	CANTalon reel;
 	
+	public Grappler(){
+		//change IDs
+		arm = new CANTalon(0);
+		elevator = new CANTalon(0);
+		reel = new CANTalon(0);
+	}
+	public void teleop(){
+		//wait for an input then move motors
+		
+	}
+	public void armLaunchMotor() {
+		//1st stage
+	}
+	public void elevatorMotor() {
+		//2nd stage
+		
+	}
+	public void reelingMotor() {
+		//3rd stage: 2 feet up
+	}
 }
