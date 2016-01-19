1ca6d1f35f0e09402344f4d1d58537976c74f6c9
team2473 <frc2473@gmail.com>
2016-01-18 17:03:47 -0800

    Added motors to methods

--- Shooter.java
+++ Shooter.java
@@ -2,15 +2,29 @@ package org.usfirst.frc.team2473.robot;
 
 public class Shooter {
 	public void pickUp(){
-		
+		Motor.moveShooterLever(255); //temp value		
+		while(ballLoaded() == false){
+			Motor.spinShooter(0.5);
+		}
+		Motor.moveShooterLever(0); //return to default position
 	}
 	
 	public void score(){
-		
+		Motor.moveShooterLever(255); //temp value
+		spinShooter(-0.5);		
+	}
+	
+	public void reset(){
+		Motor.moveShooterLever(0); //default value
+		spinShooter(0); 
 	}
 	//using lidar
 	private boolean ballLoaded(){
 		return false;
+	}
+	
+	public void teleOp(){
+		
 	}
 	
 }
