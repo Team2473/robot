11f633fb010dd07232ee079db6100ddd5d20e447
team2473 <frc2473@gmail.com>
2016-01-18 16:51:50 -0800

    Pick up, score, and loading methods added

--- Shooter.java
+++ Shooter.java
@@ -1,5 +1,16 @@
 package org.usfirst.frc.team2473.robot;
 
 public class Shooter {
-
+	public void pickUp(){
+		
+	}
+	
+	public void score(){
+		
+	}
+	//using lidar
+	private boolean ballLoaded(){
+		return false;
+	}
+	
 }
