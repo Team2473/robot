2b6f8d570b4ed6f53e0cee86b4500bb89e6c8963
team2473 <frc2473@gmail.com>
2016-01-20 19:27:16 -0800

    Joystick getX/Y

--- Controller.java
+++ Controller.java
@@ -1,5 +1,23 @@
 package org.usfirst.frc.team2473.robot;
 
-public class Controller {
+import edu.wpi.first.wpilibj.Joystick;
 
+public class Controller {
+	
+	Joystick joy1;
+	Joystick joy2;
+	
+	public Controller(){
+		joy1 = new Joystick(0); //adjust?
+		joy2 = new Joystick(1); //adjust?
+	}
+	
+	public double getX(){
+		return joy1.getX();
+	}
+	public double getY(){
+		return joy1.getY();
+	}
+	
+	
 }
