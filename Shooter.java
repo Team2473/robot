577d99e2cff9268f0f3d1647fad6c46f309b7351
Ekta Shahani <shahani.ekta@gmail.com>
2016-01-20 19:44:06 -0800

    Read potentiometer

--- Shooter.java
+++ Shooter.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2473.robot;
 
 public class Shooter {
-
+	AnalogPotentiometer pot = new AnalogPotentiometer(0); //channel 0;
+	
+	SmartDashboard.putString("DB/String 3", "Potentiometer", + pot.get());
 }
