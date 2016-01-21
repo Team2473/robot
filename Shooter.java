e07e4064e8a0b6c9310fa1a9f2cd84f39933e9a9
Cupertino High School Robotics <frc2473@gmail.com>
2016-01-20 20:14:26 -0800

    Potentiometer display test

--- Shooter.java
+++ Shooter.java
@@ -1,7 +1,12 @@
 package org.usfirst.frc.team2473.robot;
 
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Shooter {
-	AnalogPotentiometer pot = new AnalogPotentiometer(0); //channel 0;
-	
-	SmartDashboard.putString("DB/String 3", "Potentiometer", + pot.get());
+
+	static AnalogPotentiometer pot = new AnalogPotentiometer(0); //channel 0;
+	public static void testPotentiometer(){	
+		SmartDashboard.putString("DB/String 0", "Potentiometer" + pot.get());			
+	}
 }
