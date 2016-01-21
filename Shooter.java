8ca560a61a50f96b895484df7c2004cea8122d3f
Ekta Shahani <shahani.ekta@gmail.com>
2016-01-20 20:22:15 -0800

    Adding potentiometer methods

--- Shooter.java
+++ Shooter.java
@@ -5,8 +5,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter {
 
-	static AnalogPotentiometer pot = new AnalogPotentiometer(0); //channel 0;
+	private static AnalogPotentiometer pot = new AnalogPotentiometer(0); //channel 0;
+	
 	public static void testPotentiometer(){	
 		SmartDashboard.putString("DB/String 0", "Potentiometer" + pot.get());			
+	}
+	
+	public static void pickUp(){
+		moveShooterLever(0.5); //potentiometer value
+		
 	}
 }
