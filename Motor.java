552a28d85cd32339ec88e1de89cf192f784376d7
Cupertino High School Robotics <frc2473@gmail.com>
2016-01-22 14:43:41 -0800

    Added singleton code

--- Motor.java
+++ Motor.java
@@ -6,11 +6,19 @@ import edu.wpi.first.wpilibj.CANTalon.FeedbackDevice;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Motor {
-	private static CANTalon frontRight;
-	private static CANTalon frontLeft;
-	private static CANTalon backRight;
-	private static CANTalon backLeft;
-	
+	private CANTalon frontRight;
+	private CANTalon frontLeft;
+	private CANTalon backRight;
+	private CANTalon backLeft;
+
+	private CANTalon arm; // the motor to move the arm into position
+	private CANTalon elevator; // the motor to extend the arm
+	private CANTalon grappler; // the motor to grab onto the rung and pull up
+
+	private CANTalon shooterLever; // pick up arm for shooter
+	private CANTalon spinner1; // spinners to grab ball
+	private CANTalon spinner2;
+
 	// add addition cantalons as they are added to robot
 	public static final ControlMode MODE_POWER = ControlMode.PercentVbus;
 	public static final ControlMode MODE_POSITION = ControlMode.Position;
@@ -18,105 +26,129 @@ public class Motor {
 
 	// add more modes as necessary
 
-	public static void motorInit() {
+	private static Motor motor = null;
+
+	private Motor() {
 		frontRight = new CANTalon(3);
 		frontLeft = new CANTalon(2);
 		backRight = new CANTalon(4);
 		backLeft = new CANTalon(5);
-		
-		//test ids
+
+		// test ids
+		arm = new CANTalon(0);
+		elevator = new CANTalon(0);
+		grappler = new CANTalon(0);
+
+		shooterLever = new CANTalon(0);
+		spinner1 = new CANTalon(0);
+		spinner2 = new CANTalon(0);
 
 		setUpDriveMotors(frontRight);
 		setUpDriveMotors(frontLeft);
 		setUpDriveMotors(backRight);
 		setUpDriveMotors(backLeft);
 
-		// add addition cantalons as they are added to robot
+		setUpArm(arm);
+		setUpElevator(elevator);
+		setUpGrappler(grappler);
 
+		setUpShooterLever(shooterLever);
+		setUpSpinners(spinner1);
+		setUpSpinners(spinner2);
+		// add addition cantalons as they are added to robot
+	}
+
+	public static Motor getInstance() {
+		if (motor == null) {
+			motor = new Motor();
+		}
+		return motor;
 	}
 
 	// Should only run once for each cantalon
-	private static void setUpDriveMotors(CANTalon tal) {
+	private void setUpDriveMotors(CANTalon tal) {
 		tal.changeControlMode(ControlMode.Position);
 		tal.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-		tal.setPID(.1,0,0); //test pid values
+		tal.setPID(.1, 0, 0); // test pid values
 		tal.setPosition(0);
 		tal.enableControl();
 	}
-	
-	private static void setUpArm(CANTalon tal) {
+
+	private void setUpArm(CANTalon tal) {
 		tal.changeControlMode(ControlMode.Position);
 		tal.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-		tal.setPID(.1,0,0); //test pid values
+		tal.setPID(.1, 0, 0); // test pid values
 		tal.setPosition(0);
 		tal.enableControl();
 	}
-	
-	private static void setUpElevator(CANTalon tal) {
+
+	private void setUpElevator(CANTalon tal) {
 		tal.changeControlMode(ControlMode.Position);
 		tal.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-		tal.setPID(.1,0,0); //test pid values
+		tal.setPID(.1, 0, 0); // test pid values
 		tal.setPosition(0);
 		tal.enableControl();
 	}
-	
-	private static void setUpGrappler(CANTalon tal) {
+
+	private void setUpGrappler(CANTalon tal) {
 		tal.changeControlMode(ControlMode.Position);
 		tal.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-		tal.setPID(.1,0,0); //test pid values
+		tal.setPID(.1, 0, 0); // test pid values
 		tal.setPosition(0);
 		tal.enableControl();
 	}
-	
-	
-	private static void setUpShooterLever(CANTalon tal) {
+
+	private void setUpShooterLever(CANTalon tal) {
 		tal.changeControlMode(ControlMode.Voltage);
 		tal.setFeedbackDevice(FeedbackDevice.AnalogPot);
-		tal.setPID(.1,0,0); //test pid values
+		tal.setPID(.1, 0, 0); // test pid values
 		tal.setPosition(0);
 		tal.enableControl();
 	}
-	
-	private static void setUpSpinners(CANTalon tal) {
+
+	private void setUpSpinners(CANTalon tal) {
 		tal.changeControlMode(ControlMode.PercentVbus);
 		tal.setPosition(0);
 		tal.enableControl();
 	}
-	
-	public static void moveLeftSideMotors(double value) {
-		if(frontLeft.getControlMode() == MODE_POWER){
+
+	public void moveLeftSideMotors(double value) {
+		if (frontLeft.getControlMode() == MODE_POWER) {
 			frontLeft.set(value);
 			backLeft.set(value);
-		}else if(frontLeft.getControlMode() == MODE_POSITION){
+		} else if (frontLeft.getControlMode() == MODE_POSITION) {
 			frontLeft.set(value);
-			backLeft.set(2);//frontleft integer id
+			backLeft.set(2);// frontleft integer id
 		}
-		SmartDashboard.putString("DB/String 2", "FL: " + frontLeft.getEncPosition() );
+		SmartDashboard.putString("DB/String 2",
+				"FL: " + frontLeft.getEncPosition());
 	}
 
-	public static void setLeftSideMotorsMode(ControlMode mode) {
+	public void setLeftSideMotorsMode(ControlMode mode) {
 		if (mode == MODE_POWER) {
 			frontLeft.changeControlMode(mode);
 			backLeft.changeControlMode(mode);
 		} else if (mode == MODE_POSITION) {
 			frontLeft.changeControlMode(MODE_POSITION);
+			// set pid for front left
 			backLeft.changeControlMode(ControlMode.Follower);
 		}
 	}
 
-	public static void moveRightSideMotors(double value) {
-		if(frontRight.getControlMode() == MODE_POWER){
+	public void moveRightSideMotors(double value) {
+		if (frontRight.getControlMode() == MODE_POWER) {
 			frontRight.set(-value);
 			backRight.set(-value);
-		}else if(frontRight.getControlMode() == MODE_POSITION){
+		} else if (frontRight.getControlMode() == MODE_POSITION) {
 			frontRight.set(value);
-			backRight.set(3);//frontright integer id
+			backRight.set(3);// frontright integer id
 		}
-		
-		SmartDashboard.putString("DB/String 3", "FR: " + frontRight.getEncPosition() );
+
+		SmartDashboard.putString("DB/String 3",
+				"FR: " + frontRight.getEncPosition());
 	}
 
-	public static void setRightSideMotorsMode(ControlMode mode) {
+	public void setRightSideMotorsMode(ControlMode mode) {
 		if (mode == MODE_POWER) {
 			frontRight.changeControlMode(mode);
 			backRight.changeControlMode(mode);
@@ -126,27 +158,41 @@ public class Motor {
 			backRight.changeControlMode(ControlMode.Follower);
 			backRight.reverseOutput(true);
 		}
-		
-		
+
+	}
+
+	public void moveGrapplerArmMotor(double value) {
+		arm.set(value);
+	}
+
+	public void moveGrapplerElevatorMotor(double value) {
+		elevator.set(value);
+	}
+
+	public void moveGrapplerMotor(double value) {
+		grappler.set(value);
+	}
+
+	public void moveShooterLever(double value) {
+		shooterLever.set(value);
+	}
+
+	public void spinShooter(double value) { // runs on speed
+		spinner1.set(value);
+		spinner2.set(value);
 	}
 
 	// create additional move methods using the below format
 	/*
-	public static void moveSAMPLE_MOTORMotors(ControlMode mode, int value) {
-		if(SAMPLE_MOTOR.getControlMode() == MODE_POWER){
-			SAMPLE_MOTOR.set(value);
-		}else if(SAMPLE_MOTOR.getControlMode() == MODE_POSITION){
-			SAMPLE_MOTOR.set(value);
-		}
-
-	}
-
-	public static void setSAMPLE_MOTORMotorsMode(ControlMode mode) {
-		if (mode == MODE_POWER) {
-			SAMPLE_MOTOR.changeControlMode(mode);
-		} else if (mode == MODE_POSITION) {
-			SAMPLE_MOTOR.changeControlMode(MODE_POSITION);
-		}
-	}
+	 * public void moveSAMPLE_MOTORMotors(ControlMode mode, int value) {
+	 * if(SAMPLE_MOTOR.getControlMode() == MODE_POWER){ SAMPLE_MOTOR.set(value);
+	 * }else if(SAMPLE_MOTOR.getControlMode() == MODE_POSITION){
+	 * SAMPLE_MOTOR.set(value); }
+	 * 
+	 * }
+	 * 
+	 * public void setSAMPLE_MOTORMotorsMode(ControlMode mode) { if (mode ==
+	 * MODE_POWER) { SAMPLE_MOTOR.changeControlMode(mode); } else if (mode ==
+	 * MODE_POSITION) { SAMPLE_MOTOR.changeControlMode(MODE_POSITION); } }
 	 */
 }
