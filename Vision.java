8f28fc5d4535d791f57700fca14c47955dc6ea11
visatish <vishalstw@gmail.com>
2016-01-22 16:09:28 -0800

    More comments
    more comments

--- Vision.java
+++ Vision.java
@@ -38,7 +38,7 @@ public class Vision {
         NIVision.IMAQdxConfigureGrab(session);
         
         //create rectangle
-//        rect = new NIVision.Rect(50, 100, 100, 200);
+//        rect = new NIVision.Rect(ypos, xpos, height, width); x, y top left
         rect = new NIVision.Rect(50, 100, 100, 200);
         
         //start accquisition
