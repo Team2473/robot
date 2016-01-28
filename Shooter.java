e2fc1a9d5c963f218592474232c1c194f886fd37
Cupertino High School Robotics <frc2473@gmail.com>
2016-01-27 19:27:58 -0800

    Added IR

--- Shooter.java
+++ Shooter.java
@@ -1,22 +1,24 @@
 package org.usfirst.frc.team2473.robot;
 
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter {
 
 	//private static AnalogPotentiometer pot = new AnalogPotentiometer(0); //channel 0;
-	private static AnalogInput IR = new AnalogInput(1);
+	private static AnalogInput IR = new AnalogInput(0);
 	
 	public static void testPotentiometer(){	
 		//SmartDashboard.putString("DB/String 0", "Potentiometer" + pot.get());		
-		SmartDashboard.putString("DB/String 0", "IR" + IR.getVoltage());
+		SmartDashboard.putString("DB/String 0", "IR: " + IR.getVoltage());
 	}
 	
 	public void pickUp(){
 		moveShooterLever(0.5); //potentiometer value, ground level
-		//need to add while loop using lidar
-		spinShooter(0.5); //speed value
+		//test value
+		while(IR.getVoltage() < 2.3){
+			spinShooter(0.5); //speed value
+		}
 		moveShooterLever(0.2); //carrying position
 	}
 	
@@ -29,5 +31,5 @@ public class Shooter {
 	public void reset(){
 		spinShooter(0); 
 		moveShooterLever(0); //up position
-	}
+	} 
 }
