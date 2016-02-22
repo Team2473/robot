4b385bdd1e94b17e9c88602f5a12127c2146a4e4
visatish <vishalstw@gmail.com>
2016-02-21 21:50:36 -0800

    fixed conflicts

--- Shooter.java
+++ Shooter.java
@@ -18,10 +18,6 @@ public class Shooter {
 		FIRING
 	};
 	
-<<<<<<< HEAD
-	
-=======
->>>>>>> master
 	private static String stateString(State state){
 		switch(state){
 		case COLLAPSED:
@@ -52,14 +48,9 @@ public class Shooter {
 	public static int backPotMax = 420;
 	
 	// Joystick Mapping
-<<<<<<< HEAD
 	public static int loadButton     = 4;
 	public static int unloadButton   = 5;
 	public static boolean abortShoot = false;
-=======
-	public static int loadButton   = 4;
-	public static int unloadButton = 5;
->>>>>>> master
 	
 	// Input
 	public static DigitalInput breakBeam   = new DigitalInput(0); 
@@ -77,23 +68,6 @@ public class Shooter {
 		shootL.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
 	}
 	
-<<<<<<< HEAD
-	//Pot reading for positioning
-	public static void testPot(){
-		SmartDashboard.putString("DB/String 1", "" + pot.getAnalogInRaw());	
-		if(joystick.getRawButton(2)){
-			moveBackward();
-		}
-		else if(joystick.getRawButton(3)){
-			moveForward();
-		}
-		else{
-			stop();
-		}
-	}
-	
-
-=======
 	//Button breakdown
 	public static void spinIn(){
 		if(joystick.getRawButton(6)){
@@ -133,9 +107,7 @@ public class Shooter {
 			setPosition(90);
 		}
 	}
-		
-	
->>>>>>> master
+
 	// Basic power instructions
 	
 	public static void moveForward(){
@@ -184,18 +156,6 @@ public class Shooter {
 		setPosition(0);
 	}
 
-<<<<<<< HEAD
-	
-	
-	// Loading and Unloading
-	public static boolean fire(){
-		if(currentState == State.RAISED || currentState == State.RAISING){
-			currentState = State.LOWERING;
-		}
-		return true;
-	}
-	
-=======
 	// Loading and Unloading
 	
 	
@@ -206,8 +166,7 @@ public class Shooter {
 		}
 		return true;
 	}
-	
->>>>>>> master
+
 	//Get to extend state
 	public static boolean extend(){
 		if(currentState == State.COLLAPSED || currentState == State.COLLAPSING){
@@ -239,10 +198,6 @@ public class Shooter {
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
-<<<<<<< HEAD
-=======
-			// TODO Auto-generated catch block
->>>>>>> master
 			e.printStackTrace();
 		}
 	}
@@ -261,21 +216,9 @@ public class Shooter {
 			collapse();
 		}
 		//get status of fire button
-<<<<<<< HEAD
-		if(joystick.getRawAxis(2) == 1){
-			fire();
-		}
-		
-		//safety abort
-		if(joystick.getRawButton(2)){
-			if(currentState == State.LOWERING) abortShoot = true;
-		}
-		
-=======
 		if(joystick.getRawButton(6)){
 			fire();
 		}
->>>>>>> master
 	}
 	
 	public static void runLoop(){
@@ -320,14 +263,8 @@ public class Shooter {
 		else if(currentState == State.RAISED){
 			pot.set(0); //This is temporary, need to fine tune table values
 		}
-<<<<<<< HEAD
-		//firing, abort shoot
-		else if(currentState == State.LOWERING){
-			if(isExtended() || abortShoot){ 
-=======
 		else if(currentState == State.LOWERING){
 			if(isExtended()){
->>>>>>> master
 				currentState = State.FIRING;
 			}
 			else{
@@ -340,10 +277,6 @@ public class Shooter {
 			}
 			else{
 				fireBall();
-<<<<<<< HEAD
-				abortShoot = false;
-=======
->>>>>>> master
 			}
 		}
 	}
@@ -360,11 +293,6 @@ public class Shooter {
 		return getPosition() <= 90; 
 	}
 	
-<<<<<<< HEAD
-		
-	
-=======
->>>>>>> master
 	//DO NOT USE. Saving for use in test program.
 	public static void load(){
 		boolean continueMethod = true;
@@ -436,14 +364,7 @@ public class Shooter {
 	// Motor Control 
 	// TODO: This should be using the motor class
 	
-<<<<<<< HEAD
-	public static double[] lookupTable = {0.34, 0.32, 0.32, 0.30, 0.28, 0.26, 0.26, 0.26, 0.24, 0.24, 0.22, 0.22, 0.20, 0.19, 0.16, 0.14, 0.14, 0.12, 0.10, 0.02};  
-=======
-	
-	
-	
 	public static double[] lookupTable = {0.28, 0.26, 0.23, 0.22, 0.21, 0.20, 0.19, 0.18, 0.17, 0.17, 0.16, 0.15, 0.12, 0.08, 0.08, 0.08, 0.08, 0.06, 0.06, 0.02};  
->>>>>>> master
 
 	public static double getPosition(){
 		double diff = fwdPotMax - backPotMax;
